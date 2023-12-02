a0c855bdfa7b67524e11bf72d40643e988888baf
SamuelFuessinger <92742829+Samsu-F@users.noreply.github.com>
2023-12-02 17:02:18 +0100

    fix yesterdays code using test data

--- 01/01.1.sh
+++ 01/01.1.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+# shellcheck disable=SC2002
 
-cat ./test | \
+cat ./input | \
     sed -E 's/^[^0-9]*([0-9]).*([0-9])[^0-9]*$/\1\2/g' |   `# only keep first and last digit if there are two or more digit in the line` \
     sed -E 's/^[^0-9]*([0-9])[^0-9]*$/\1\1/g' |   `# only keep the single digit but duplicate it if there is only one digit in the line` \
     paste -sd+ |   `# put everything in one line separated by '+'` \
