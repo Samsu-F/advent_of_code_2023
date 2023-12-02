2bea321730eb02bac8afda4aa4a395c1efa3ab2b
SamuelFuessinger <92742829+Samsu-F@users.noreply.github.com>
2023-12-02 17:00:18 +0100

    add solution 02

---
+++ 02/02.1.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# shellcheck disable=SC2002 # disable 'useless cat' warning
+
+cat ./input | \
+    grep -Ev '(1[3-9]|[2-9][0-9]|[1-9][0-9]{2,}) red' | \
+    grep -Ev '(1[4-9]|[2-9][0-9]|[1-9][0-9]{2,}) green' | \
+    grep -Ev '(1[5-9]|[2-9][0-9]|[1-9][0-9]{2,}) blue' | \
+    grep -E --only-matching '^Game [0-9]+' | \
+    grep -E --only-matching '[0-9]+' | \
+    paste -sd+ | \
+    bc
